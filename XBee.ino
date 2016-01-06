c785986226847eeb36afbe2cb0fa66f1e298e342
Bachir31 <bouacheria.bachir@hotmail.fr>
2016-01-06 18:59:29 +0100

    [BBA] adding XBee.ino from practical session statement

---
+++ XBee.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <string.h>
+
+const int ledPin = 13;
+int incomingByte;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        if (incomingByte == 'H') {
+            digitalWrite(ledPin, HIGH);
+        }
+
+        if (incomingByte == 'L'){
+            digitalWrite(ledPin, LOW);
+        }
+    }
+}
